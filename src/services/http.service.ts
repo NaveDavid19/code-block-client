9c6aedb31efbc3b922324b5a5d6713c7a4acfd98
NaveDavid19 <navedavid16@gmail.com>
2024-09-13 14:49:56 +0300

    add http service

---
+++ src/services/http.service.ts
@@ -0,0 +1,49 @@
+import Axios from 'axios'
+
+const BASE_URL =
+  process.env.NODE_ENV === 'production' ? '/api' : '//localhost:3030/api'
+
+const axios = Axios.create({
+  withCredentials: true,
+})
+
+export const httpService = {
+  get<T>(endpoint: string, data?: T) {
+    return ajax(endpoint, 'GET', data)
+  },
+  post<T>(endpoint: string, data?: T) {
+    return ajax(endpoint, 'POST', data)
+  },
+  put<T>(endpoint: string, data?: T) {
+    return ajax(endpoint, 'PUT', data)
+  },
+  delete<T>(endpoint: string, data?: T) {
+    return ajax(endpoint, 'DELETE', data)
+  },
+}
+
+async function ajax(
+  endpoint: string,
+  method: 'GET' | 'POST' | 'PUT' | 'DELETE',
+  data: any = null
+) {
+  try {
+    const res = await axios({
+      url: `${BASE_URL}${endpoint}`,
+      method,
+      data,
+      params: method === 'GET' ? data : null,
+    })
+    return res.data
+  } catch (err: any) {
+    console.log(
+      `Had Issues ${method}ing to the backend, endpoint: ${endpoint}, with data: `,
+      data
+    )
+    console.dir(err)
+    if (err.response && err.response.status === 401) {
+      sessionStorage.clear()
+    }
+    throw err
+  }
+}
