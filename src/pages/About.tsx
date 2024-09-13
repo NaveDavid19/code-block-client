f1ffe565afb17fee071194cf6ace46d6c560e70e
NaveDavid19 <navedavid16@gmail.com>
2024-09-13 14:48:41 +0300

    add About cmp

---
+++ src/pages/About.tsx
@@ -0,0 +1,19 @@
+export function About() {
+  return (
+    <section className="about-container">
+      <h2>About </h2>
+      <img src="..\src\assets\img\nave.jpg" alt="" />
+      <p>
+        My name is Nave David. im 23 years old and I recently graduated a very
+        intensive and dense course at the Coding Academy`s Bootcamp.
+      </p>
+      <p>
+        Im looking forward to containuing to devleop my programming skills and
+        knowledge
+      </p>
+      <h1>Visit my social media</h1>
+      <a href="https://www.linkedin.com/in/nave-david-01527a2a6/">LinkedIn</a>
+      <a href="https://github.com/NaveDavid19">GitHub</a>
+    </section>
+  )
+}
