aca7c548c683b0164935c682848a76cec84e9361
NaveDavid19 <navedavid16@gmail.com>
2024-09-13 14:52:53 +0300

    refactor and optimaze blockDetails state and sockets

--- src/cmps/BlockDetails.tsx
+++ src/cmps/BlockDetails.tsx
@@ -1,78 +1,173 @@
-import { useParams } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 import { Block } from '../services/async-storage.service'
-import { useEffect, useState } from 'react'
+import { ChangeEvent, useEffect, useState } from 'react'
 import { blockService } from '../services/block.service'
 import Button from '@mui/material/Button'
-import { Link } from 'react-router-dom'
+import {
+  socket,
+  SOCKET_EVENT_CODE_EDIT,
+  SOCKET_EVENT_MENTOR_JOIN,
+  SOCKET_EVENT_MENTOR_LEFT,
+  SOCKET_EVENT_STUDENT_JOIN,
+  SOCKET_EVENT_STUDENT_LEFT,
+} from '../services/socket.service'
+import { SyntaxHighlighterCmp } from './SyntaxHighlighterCmp'
+import { utilService } from '../services/util.service'
+import smileyImg from '../assets/img/smiley-face.jpg'
 
 export const BlockDetails = () => {
-  const { blockId } = useParams()
-  const [block, setBlock] = useState<Block>({
-    _id: '',
-    initialTemplate: '',
-    isVisited: false,
-    title: '',
-  })
-  const [isMentor, setIsMentor] = useState<boolean>()
+  const navigate = useNavigate()
+  const { codeBlockId } = useParams<{ codeBlockId: string }>()
+  const [isMentor, setIsMentor] = useState<boolean | undefined>()
+  const [block, setBlock] = useState<Block>(blockService.emptyBlock)
+  const [codeTest, setCodeTest] = useState<boolean>(false)
 
   useEffect(() => {
-    blockId && loadBlock(blockId)
-  }, [blockId])
+    socket?.on('increase-watchers', (v) => updateWatchers(v + 1))
+    socket?.on('decrease-watchers', (v) => updateWatchers(v - 1))
+    socket?.on('mentor-join', (v) => updateWatchers(v + 1))
+    socket?.on('mentor-left', mentorLeft)
+    socket?.on('code-changed', updateCode)
 
-  const loadBlock = async (blockId: string): Promise<void> => {
+    return () => {
+      socket?.off('increase-watchers')
+      socket?.off('decrease-watchers')
+      socket?.off('mentor-join')
+      socket?.off('mentor-left')
+      socket?.off('code-changed')
+    }
+  }, [block])
+
+  useEffect(() => {
+    if (codeBlockId && !block._id) {
+      loadBlock(codeBlockId)
+    }
+  }, [codeBlockId])
+
+  const loadBlock = async (id: string): Promise<void> => {
     try {
-      let selectedBlock = await blockService.getById(blockId)
-      const isMentor = !selectedBlock.isVisited
-
-      if (isMentor) {
-        selectedBlock = { ...selectedBlock, isVisited: true }
-        await blockService.save(selectedBlock)
+      console.log('Loading block..')
+      const selectedBlock = await blockService.getById(id)
+      if (!selectedBlock.visitorCounter) {
+        setIsMentor(true)
+        selectedBlock.value = selectedBlock.initialTemplate
+        socket?.emit(SOCKET_EVENT_MENTOR_JOIN, {
+          msg: 'mentor connected to block',
+          data: selectedBlock.visitorCounter,
+        })
+      } else {
+        socket?.emit(SOCKET_EVENT_STUDENT_JOIN, {
+          msg: 'student connected to block',
+          data: selectedBlock.visitorCounter,
+        })
       }
-
-      setIsMentor(isMentor)
+      selectedBlock.visitorCounter++
       setBlock(selectedBlock)
+
+      await blockService.save(selectedBlock)
     } catch (err) {
       console.error('Failed to load block:', err)
     }
   }
 
-  const onLeaveBlock = () => {
+  const updateWatchers = (value: number) => {
+    setBlock((prevBlock) => {
+      const updatedBlock: Block = { ...prevBlock, visitorCounter: value }
+      blockService.save(updatedBlock)
+      return updatedBlock
+    })
+  }
+
+  const updateCode = (value: string) => {
+    setBlock((prevBlock) => ({ ...prevBlock, value }))
+  }
+
+  const mentorLeft = async () => {
+    console.log('Mentor left, resetting block')
+    const updatedBlock = {
+      ...block,
+      visitorCounter: 0,
+      value: block.initialTemplate,
+    }
+    setBlock(updatedBlock)
+    try {
+      await blockService.save(updatedBlock)
+    } catch (err) {
+      console.error('Failed to save block after mentor left:', err)
+    }
+    navigate('/codeBlock')
+  }
+
+  const onLeaveBlock = async () => {
     if (isMentor) {
-      blockService.save({ ...block, isVisited: false })
-      //TODO : redirect all users to path '/'
+      socket?.emit(SOCKET_EVENT_MENTOR_LEFT, {
+        msg: `mentor left the block`,
+      })
+    } else {
+      socket?.emit(SOCKET_EVENT_STUDENT_LEFT, {
+        msg: `student left the block`,
+        data: block.visitorCounter,
+      })
+      navigate('/codeBlock')
+    }
+  }
+
+  const handleCodeChange = async (e: ChangeEvent<HTMLDivElement>) => {
+    e.preventDefault()
+    const { innerText } = e.target
+    setBlock((prevBlock) => ({ ...prevBlock, value: innerText }))
+    try {
+      await blockService.save({ ...block, value: innerText })
+      socket?.emit(SOCKET_EVENT_CODE_EDIT, {
+        msg: 'code has changed',
+        data: innerText,
+      })
+    } catch (err) {
+      console.error('Failed to save block:', err)
+    }
+  }
+
+  const handleTestBlock = (txt: string) => {
+    if (
+      utilService.processString(txt) ===
+      utilService.processString(block.solution)
+    ) {
+      setCodeTest(true)
+      setTimeout(() => setCodeTest(false), 5000)
     }
   }
 
   return (
     <>
+      {codeTest && (
+        <img className="smiley-emoji" src={smileyImg} alt="Smiley face" />
+      )}
       <Button
         onClick={onLeaveBlock}
         size="small"
-        style={{ marginTop: '8px ', backgroundColor: 'black', width: '50px' }}
+        style={{ marginTop: '8px', backgroundColor: 'black', width: '50px' }}
         variant="contained"
-        component={Link}
-        to={`/`}
       >
-        {' '}
         Back
-      </Button>{' '}
-      <h1>{isMentor ? 'mentor' : 'student'}</h1>
-      <h2 style={{ textDecoration: 'underline' }}>{block?.title}</h2>
-      <div className="code-container">
-        <textarea
-          readOnly={isMentor}
-          defaultValue={block?.initialTemplate}
-          //   value={block?.value ?? block?.initialTemplate}
-        />
-      </div>
-      <Button
-        size="small"
-        style={{ margin: '8px 0', backgroundColor: 'black', width: '50px' }}
-        variant="contained"
-        // onClick={handleSave}
-      >
-        Save
       </Button>
+      <h1>Role: {isMentor ? 'Mentor' : 'Student'}</h1>
+      <h4>Watching: {block.visitorCounter}</h4>
+      <h2 style={{ textDecoration: 'underline' }}>{block.title}</h2>
+      <div className="code-container">
+        <SyntaxHighlighterCmp
+          handleCodeChange={utilService.debounce(handleCodeChange, 500)}
+          isMentor={isMentor ?? false}
+          txt={block.value ?? block.initialTemplate}
+        />
+        <Button
+          onClick={() => handleTestBlock(block.value)}
+          size="small"
+          style={{ marginTop: '8px', backgroundColor: 'black', width: '50px' }}
+          variant="contained"
+        >
+          Test
+        </Button>
+      </div>
     </>
   )
 }
