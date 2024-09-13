a197590d73719727e4c5e56ac2bf1d03f8a82ca3
NaveDavid19 <navedavid16@gmail.com>
2024-09-13 14:50:38 +0300

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,20 @@ import { Route, Routes, BrowserRouter as Router } from 'react-router-dom'
 import './assets/style/main.scss'
 import { LobbyPage } from './pages/LobbyPage'
 import { BlockDetails } from './cmps/BlockDetails'
-
+import { Home } from './pages/Home'
+import { AppHeader } from './cmps/AppHeader'
+import { About } from './pages/About'
 export function App() {
   return (
     <Router>
       <section className="main-layout">
-        {/* <AppHeader/> */}
+        <AppHeader />
         <main className="main-layout full">
           <Routes>
-            <Route element={<LobbyPage />} path="/" />
-            <Route element={<BlockDetails />} path="/block/:blockId" />
+            <Route element={<Home />} path="/" />
+            <Route element={<LobbyPage />} path="/codeBlock" />
+            <Route element={<BlockDetails />} path="/codeBlock/:codeBlockId" />
+            <Route element={<About />} path="/about" />
           </Routes>
         </main>
       </section>
